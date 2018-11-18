305ae3bd1b39343257f95569404860eafab09c5d
sangmin <sangmins39@gmail.com>
2018-11-18 15:15:15 +0900

    init app

---
+++ app/main/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from . import main
+
+@main.route('/')
+def home():
+    return render_template('home.html')
+
+@main.route('/about')
+def about():
+    return render_template('about.html')
