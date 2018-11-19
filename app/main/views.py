d5b420d46a0ef314e765eb92a60dc985ba66cdf8
sangmin <sangmins39@gmail.com>
2018-11-19 23:34:17 +0900

    Big update

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,32 @@
-from flask import render_template
+from flask import render_template, session, redirect, url_for, flash, current_app
 from . import main
+from .forms import NameForm
+from ..email import send_email
+from .. import db
+from ..models import User
 
-@main.route('/')
+
+@main.route('/',methods=['GET','POST'])
 def home():
-    return render_template('home.html')
+    form=NameForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user=User(username=form.name.data)
+            db.session.add(user)
+            session['known']=False
+            
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
+        else:
+            session['known']=True
+        session['name']=form.name.data
+        return redirect(url_for('main.home'))
+    return render_template('home.html', 
+                            form=form, 
+                            name=session.get('name'),
+                            known=session.get('known', False))
 
 @main.route('/about')
 def about():
