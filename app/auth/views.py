448b1102aed1b527ecf49b0f66aa5b3c7588c6d5
sangmin <sangmins39@gmail.com>
2018-11-22 19:50:07 +0900

    Add register view

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from . import auth
-from ..models import User
-from .forms import LoginForm
+from ..models import User, db
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['POST','GET'])
 def login():
@@ -14,6 +14,18 @@ def login():
             return redirect(request.args.get('next') or url_for('main.home'))
         flash('Invaild username or password')
     return render_template('auth/login.html', form=form)
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        user=User(email=form.email.data,
+                username=form.username.data,
+                password=form.password.data)
+        db.session.add(user)
+        flash('You can now login')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
 
 @auth.route('/logout')
 @login_required
